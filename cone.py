b4c0bdaa5a912c90e3dcab54ba51072337ed2d65
Naga321 <97340710+Naga321@users.noreply.github.com>
2022-01-15 10:10:33 +0600

    Add files via upload

---
+++ cone.py
@@ -0,0 +1,5 @@
+h=int(input("enter the height"))
+r=int(input("enter radius"))
+pi=3.14
+volume = 1/3*pi*r*r*h
+print('the volume of cone of radius of {} and height of {} is {}:'.format(r,h,volume))
